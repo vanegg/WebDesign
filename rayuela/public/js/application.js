99fc94e45d91df757e9ee6f6602fa857567b0399
codea_mac_04 <codea_mac_04@gmail.com>
2016-09-21 12:58:29 -0500

    add keys to stop and color

--- rayuela/public/js/application.js
+++ rayuela/public/js/application.js
@@ -1,27 +1,60 @@
 $(document).ready(function() {
 
   $("#start_btn").on("click", function(){
+    restart();
     count();
    });
 
+  function restart(){
+    $("#result").text("");
+    $("td").removeClass("active");
+    $("td:nth-child(2)").addClass("active");
+    $(".table tr").removeClass("winner");
+}
+
+$("#start_btn").on( "mouseenter mouseleave", function() {
+    console.log( "mouse hovered over or left a div" );
+});
+
 function count(){
+  $("#index").show();
   count_index = parseInt($("#index").text());
+  if (!!count_index == false && count_index != 0){
+  count_index = 4;
+  }
    if (count_index == 0) {
-     move();
+    $("#index").text("¡Empieza!");
+     move1();
+     move2();
    } else {
     $("#index").text(count_index - 1);
     setTimeout(count,1000);
    }
 }
 
-function move(){
-  if ( parseInt($("td.active").attr("id")) < 100) {
-    next_td = $("td.active").next();
-    $("td").removeClass("active");
+
+function move1(){
+  $("#start_btn").text("Reiniciar");
+  finish1 = false;
+  if ( parseInt($("#Player1 td.active").attr("id")) < 100) {
+    next_td = $( "#Player1 td.active").next();
+    $( "#Player1 td").removeClass("active");
     next_td.addClass("active");
-    setTimeout(move,50);
+    timeout1 = setTimeout(move1, 25);
    } else {
-    result();
+    finish1 = true;
+   }
+}
+
+function move2(){
+  finish2 = false;
+  if ( parseInt($("#Player2 td.active").attr("id")) < 100) {
+    next_td = $( "#Player2 td.active").next();
+    $( "#Player2 td").removeClass("active");
+    next_td.addClass("active");
+    timeout2 = setTimeout(move2, 25);
+   } else {
+    finish2 = true;
    }
 }
 
@@ -29,22 +62,43 @@ function result(){
   id_player1 = parseInt($("#Player1 td.active").attr("id"));
   id_player2 = parseInt($("#Player2 td.active").attr("id"));
   id_win = parseInt($("td.meta").attr("id"));
-  dist_1 = abs(id_player1 - id_win)
-  dist_2 = abs(id_player2 - id_win)
+  dist_1 = Math.abs(id_player1 - id_win);
+  dist_2 = Math.abs(id_player2 - id_win);
+
   if (id_player1 == id_player2 && id_player1 == 100){
-    $("#result").text("Ambos jugadores perdieron")
-
-     //AQUI ME QUEDEEEEEEEEEEE!!!!
+    $("#result").text("¡Ambos jugadores perdieron!");
   } else if (dist_1 < dist_2){
+    $("#result").text("Ganador: ¡Jugador 1!");
+    $(".table tr").removeClass("winner");
+    $("#Player1").addClass("winner");
+  } else if (dist_1 > dist_2){
+    $("#result").text("Ganador: ¡Jugador 2!");
+    $(".table tr").removeClass("winner");
+    $("#Player2").addClass("winner");
+  } else {
+    $("#result").text("¡Empate!");
+  }
+}
 
+function finished_game(){
+  console.log("ENTRO A FINISHED_GAME");
+  console.log("finish1:" + finish1);
+  console.log("finish2:" + finish2);
+  if(finish1 && finish2){
+    result();
   }
 }
 
 $(document).on('keyup', function(event) {
     if ( event.which == 65){
+      clearTimeout(timeout1);
+      finish1 = true;
+      finished_game();
     }
     if ( event.which == 76 ){
-      alert( "JUGADOR 2" );
+     clearTimeout(timeout2);
+     finish2 = true;
+      finished_game();
     }
 });
 
