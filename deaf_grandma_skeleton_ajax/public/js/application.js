d4dbe5275402c57a145dcd7492c2bdc5c48ec163
codea_mac_04 <codea_mac_04@gmail.com>
2016-09-21 20:54:56 -0500

    add view statistics maraton

--- deaf_grandma_skeleton_ajax/public/js/application.js
+++ deaf_grandma_skeleton_ajax/public/js/application.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   $("form").on("submit", function(event) {
     event.preventDefault();
     data = $(this).serialize();
+    console.log(this);
+    console.log(data);
     $.post('/abuelita',data).done(function(data) {
       $("#grandma_ajax").html("Tu Abuelita dice:" + data);
     })
--- maraton_skeleton/app/controllers/game.rb
+++ maraton_skeleton/app/controllers/game.rb
@@ -2,34 +2,40 @@ get '/:user_id/play/:deck_name/:num_card' do
   @user_id = params[:user_id]
   @deck = Deck.find_by(name: params[:deck_name])
   @deck.rounds << User.find(@user_id).rounds.last
-  user = User.find(@user_id)
+  @user = User.find(@user_id)
 
-  # AQUI ME QUEDEEEE!!!!!!!!!
   if params[:answer] != "" || params[:answer] != nil
     if params[:answer] == @deck.cards[params[:num_card].to_i].answer
       puts "Respuesta Correcta"
-      intento = user.rounds.last.guesses.last
-      intento.result = true
-      intento.save()
+      guess = @user.rounds.last.guesses.last
+      guess.result = true
+      guess.save()
       @num_card = (params[:num_card].to_i + 1).to_s
-      guess = Guess.create(tries: 0, result: false)
-      user.rounds.last.guesses << guess
+      intento = Guess.create(tries: 1, result: false)
+      @user.rounds.last.guesses << intento
     else
       puts "Respuesta incorrecta"
-      guess = user.rounds.last.guesses.last
-      guess.tries += 1
-      guess.save()
-      @num_card = (params[:num_card].to_i).to_s
+      guess = @user.rounds.last.guesses.last
+      if (guess.tries < 5)
+        guess.tries += 1
+        guess.save()
+        @num_card = (params[:num_card].to_i).to_s
+      else
+        guess.save()
+        @num_card = (params[:num_card].to_i + 1).to_s
+        intento = Guess.create(tries: 1, result: false)
+        @user.rounds.last.guesses << intento
+      end
     end
   end
-   # AQUI ME QUEDEEEE!!!!!!!!!
   if @num_card.to_i < @deck.cards.length
-    @try = user.rounds.last.guesses.last.tries
-    i = params[:num_card].to_i
+    @try = @user.rounds.last.guesses.last.tries
+    i = @num_card.to_i
     @cards = @deck.cards[i]
     @answers = [@cards.answer, @cards.fake_answer1, @cards.fake_answer2].shuffle
     erb :play
   else
+
     erb :statistics
   end
 end
@@ -40,9 +46,9 @@ end
 
 post '/:user_id/play/:deck_name' do
   @user_id = params[:user_id]
-  user = User.find(@user_id)
+  @user = User.find(@user_id)
   guess = Guess.create(tries: 0, result: false)
-  user.rounds.last.guesses << guess
+  @user.rounds.last.guesses << guess
   redirect("/#{params[:user_id]}/play/#{params[:deck_name]}/0")
 end
 
