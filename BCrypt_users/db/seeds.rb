73125f9b47d35e69704b09c955e6274cd74020e5
codea_mac_04 <codea_mac_04@gmail.com>
2016-09-23 14:36:51 -0500

    Users with BCrypt"

---
+++ BCrypt_users/db/seeds.rb
@@ -0,0 +1,3 @@
+user1 = User.create(name: "Vane", email: 'van@correo.com', password: 'lalala')
+user2 = User.create(name: "Karla", email: 'karla@correo.com', password: 'lalala')
+user3 = User.create(name: "Maryel", email: 'maryel@correo.com', password: 'lalala')
