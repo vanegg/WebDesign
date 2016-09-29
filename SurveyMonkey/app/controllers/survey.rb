e02db06b8d0399197520c9114dbf2f5e68841bca
codea_mac_04 <codea_mac_04@gmail.com>
2016-09-28 21:00:16 -0500

    Answer surveys method"

--- SurveyMonkey/app/controllers/survey.rb
+++ SurveyMonkey/app/controllers/survey.rb
@@ -18,15 +18,49 @@ get '/survey/:id/create' do
 end
 
 post '/create_survey' do
+  num_op = 0
   survey = params
-  p questions = survey["questions"]
+  questions = survey["questions"]
   questions.each_with_index do |item, index|
-    name = "question" + index.to_s
-    question = Question.create(text: item[1][name])
+    key_q = "question" + index.to_s
+    question = Question.create(text: item[1][key_q])
+    options = item[1]["options"]
+    options.each do |item_op|
+      key_op = "option" + num_op.to_s
+      option = Option.create(text: item_op[1][key_op])
+      question.options << option
+      num_op += 1
+    end
     current_user.surveys.last.questions << question
-    options = item[1]
   end
+  "/users/#{current_user.id}"
+  #erb :profile
+end
 
-  # question =  Question.create(text: qs["0"][name])
-"true"
+get '/survey/answer/:id' do
+  @survey = Survey.find(params[:id])
+  erb :answer_survey
+end
+
+post '/responses' do 
+  a_params = params.to_a
+  a_params.shift
+  survey = Survey.find(params[:survey_id])
+  a_params.each do |key, value|
+    response = Response.new();
+    current_user.responses << response
+    survey.responses << response
+    new_key = key.to_s
+    new_key = new_key.gsub("response_","")
+    question = Question.find(new_key.to_i)
+    question.responses << response
+    option = Option.find(value)
+    response.option_id = value
+    response.save
+  end
+  redirect to "/users/#{current_user.id}"
+end
+
+get '/survey/results/:id' do
+  erb :result_survey
 end
--- SurveyMonkey/public/js/application.js
+++ SurveyMonkey/public/js/application.js
@@ -1,22 +1,17 @@
 $(document).ready(function() {
   i = 0; //indice para las preguntas
   j = 0; //indice para las opciones
-  var survey = [[]];
-  $("#btn_add_question").on("click", box_question);
+  $("#preview_question").append("<div class='new_question'><p><input type='text' id='q_name' size='20' name='q_name' value='' placeholder='Question'/><button id='btn_question' type='button'> Add Question</button></p></div>");
   $("#btn_complete").on("click", complete);
   $(".container").on("click", "#btn_add_option", add_option);
   $(".container").on("click", "#btn_done_question", done_question);
   $(".container").on("click", "#btn_question", box_question);
 
 function complete(){
-  console.log("Boton complete apretado");
-  json = createJSON();
-  // json = JSON.stringify()
-  console.log(json);
+  json = createJSON(); 
   //AQUI VA UN AJAX CON TODA LA INFORMACIÓN
-
-   $.post('/create_survey',json, function(data){
-    console.log("YA REGRESO EL AJAX CON: " + data)
+   $.post('/create_survey',json, function(url){
+    window.location.href = url + "";
     });
 }
 
@@ -36,54 +31,28 @@ function createJSON(){
   });
   console.log(b_question.substring(0, b_question.length - 1) + ']}');
   return JSON.parse(b_question.substring(0, b_question.length - 1) + ']}');
-
-  // return JSON.parse('{"hello": "world"}')
 }
 
 function box_question(){
-  $("#preview_question").hide();
-  $(".new_question").remove();
-  console.log("Boton add question apretado");
-  console.log($('#q_name').val());
   $("#survey_area").append("<div class='question' id='question" + i + "'><h2>"+ $('#q_name').val() + "</h2><ol><div class='option_area' id= 'option_area" + i +"''></div></ol><div class='section_option'><p><input id='o_name' type='text' size='20' name='o_name' value='' placeholder='Option'/> <button id='btn_add_option' type='button'> Add Option</button></p></div><button id='btn_done_question' type='button'> Done with question</button></div>");
   i++;
-  console.log("i:" + i);
+  $("#preview_question").remove();
+  $(".new_question").remove();
   }
 
 function add_option(){
-  console.log("Boton add option apretado");
-  console.log(i-1);
   $("#option_area" + (i - 1)).append("<li id='option" + j + "'>" + $('#o_name').val() + "</li>");
   $("#o_name").val('');
   j++;
 }
 
 function done_question(){
-  console.log("Boton done with question apretado");
   $(".section_option").remove();
   $("#survey_area").append("<div class='new_question'><p><input type='text' id='q_name' size='20' name='q_name' value='' placeholder='Question'/><button id='btn_question' type='button'> Add Question</button></p></div>");
   $(this).remove();
-  console.log("Ready")
 }
 
 function question(){
   $("#survey_area").append("<div id='new_question'><p><input type='text' id='q_name' size='20' name='q_name' value='' placeholder='Question'/><button id='btn_add_question' type='button'> Add Question</button></p></div>");
 }
 });
-
-// {"questions":[
-//               { "question1": "value",
-//                 "options": [
-//                             {"option1": "value"},
-//                             {"option2": "value"},
-//                             {"option3": "value"}
-//                             ]
-//               },
-//               { "question2":"value", 
-//                 "options": [
-//                             {"option4": "value"},
-//                             {"option5": "value"},
-//                             {"option6": "value"}
-//                             ]
-//               }
-// ]}
