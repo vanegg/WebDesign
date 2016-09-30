6b37cbba7e9561d065d89de411cf492f59dfb68e
codea_mac_04 <codea_mac_04@gmail.com>
2016-09-30 17:00:13 -0500

    Carousel of photos

---
+++ carrousel/carousel.js
@@ -0,0 +1,31 @@
+$(function() {
+  length = '=100%';
+  pos = 620;
+
+  $("#previous_frame").on("click", function(){
+    // console.log("Previous frame");
+    wl = parseInt($(".frames").css("margin-left"));
+    // console.log(wl);
+    if (wl == 0){
+      // console.log("Nueva vuelta anterior")
+      $(".frames").css("margin-left", "-300%")
+    }
+    $( ".frames" ).animate({
+      marginLeft: '+' + length,
+  }, 1500 );
+  });
+
+  $("#next_frame").on("click", function(){
+    console.log("Next frame");
+    wl = parseInt($(".frames").css("margin-left"));
+    // console.log(wl);
+    if (wl == -1860){
+      // console.log("Nueva vuelta")
+      $(".frames").css("margin-left", "0px");
+    }
+    $( ".frames" ).animate({
+      marginLeft: '-' + length,
+  }, 1500 );
+  });
+
+});
