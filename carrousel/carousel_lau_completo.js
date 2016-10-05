6b0145e8f0ca2b780221bb7c3c94c9d44146b8b3
codea_mac_04 <codea_mac_04@gmail.com>
2016-10-05 13:34:22 -0500

    Fix js carousel and add file carousel complete

--- carrousel/carousel_lau_completo.js
+++ carrousel/carousel_lau_completo.js
@@ -11,13 +11,35 @@
 
 
   //se ajusta al número de imagenes cambia width del ul para que se ajusten
-  // slider.css("width", imageNumber * 100 +'%');
+  slider.css("width", imageNumber * 100 +'%');
   //mover ultima imagen al pricipio de las imágenes
-  //para posicionar en la que era la primer imagen
-  // slider.css('margin-left', '-'+100+'%');
   $('.frames li:last').insertBefore('.frames li:first');
+  //para posicionar en la que era la primer imagen
+  slider.css('margin-left', '-'+100+'%');
 
 
+  if(imageNumber == 0){
+    $('.slider_controls').css("visibility", "hidden");
+  }  else if(imageNumber == 1){
+    //se ajusta al número de imagenes cambia width del ul para que se ajusten
+    liItems.css("width", 100 / imageNumber+'%');
+    slider.css('margin-left', 0+'%');
+    siguiente.click(function(){
+      alert("Solo has subido una imagen");
+    });
+
+    anterior.click(function(){
+      alert("Solo has subido una imagen");
+    });
+    slider.css('margin', '0%');
+  }
+  else if(imageNumber == 2){
+    //se ajusta al número de imagenes cambia width del ul para que se ajusten
+    liItems.css("width", 100 / imageNumber+'%');
+  }
+
+
+ 
 
   siguiente.click(function(){
     moverDerecha();
