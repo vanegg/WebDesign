135bb060daf62922d886826cb3134b790275a9d5
codea_mac_04 <codea_mac_04@gmail.com>
2016-10-07 17:34:32 -0500

    Catch all gets, and return the urls

--- API_twitter/public/js/application.js
+++ API_twitter/public/js/application.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
 
-  $("#search").on("click", function(event){
-    
-    //  $.post('/')
-    // $(".spinner").css("display", "block");
-   });
+  // $("#search").on("click", function(event){
+  //   event.preventDefault();    
+  //   //  $.post('/')
+  //   //$(".spinner").css("display", "block");
+  //  });
 
-// $.get().always
+
+ $.get().always(function(data){
+  url_tweet = (window.location.pathname).indexOf("/user/");
+  if(url_tweet >= 0){
+    //se redirigio a la pagina de tweets  :D
+  console.log(window.location.pathname);
+    
+  }
+ });
 
 });
