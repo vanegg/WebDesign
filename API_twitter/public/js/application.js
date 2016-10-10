0ed4be9bf9b64a5ceddb01d716449ab3b15b4f0e
codea_mac_04 <codea_mac_04@gmail.com>
2016-10-10 16:31:56 -0500

    Disable button while tweet is charging

--- API_twitter/public/js/application.js
+++ API_twitter/public/js/application.js
@@ -20,27 +20,29 @@ $(document).ready(function() {
  });
 
  $("#tweetear").on("submit", function(event){
-  event.preventDefault();
-  $('#new_tweet_section').empty();
-   tweet = $('input[name="tweet"]').val();
-   $('#new_tweet_section').append('<img id="wait" src="/flickr.gif">');
-   if (tweet.length >= 140){
-   $('#new_tweet_section').empty();
-    $('#new_tweet_section').append('<div class= "fail">Your tweet has more than 140 characters. Please try again.</div>');
-   } else {
-    t = {};
-    t.tweet = tweet;
-    $.post('/tweet',t, function(partial){
-      $('#new_tweet_section').empty();      
-      if (partial != "true"){
-        $('#new_tweet_section').append(partial);   
-      } else {
+    event.preventDefault();
+    $('input[name="btn_tweet"]').prop('disabled', true);
+    $('#new_tweet_section').empty();
+    tweet = $('input[name="tweet"]').val();
+    $('#new_tweet_section').append('<img id="wait" src="/flickr.gif">');
+    if (tweet.length >= 140){
+      $('input[name="btn_tweet"]').prop('disabled', false);
+      $('#new_tweet_section').empty();
+      $('#new_tweet_section').append('<div class= "fail">Your tweet has more than 140 characters. Please try again.</div>');
+    } else {
+      t = {};
+      t.tweet = tweet;
+      $.post('/tweet',t, function(partial){
+        $('#new_tweet_section').empty();      
+        if (partial != "true"){
+          $('#new_tweet_section').append(partial);   
+          $('input[name="btn_tweet"]').prop('disabled', false);
+        } else {
          $('#new_tweet_section').append('<div class= "fail">Something went wrong. Please try again.</div>');
-      }
-    });
-   }
-
-
- });
+         $('input[name="btn_tweet"]').prop('disabled', false);
+        }
+      });
+    }
+  });
 
 });
